1582f69328b7de2c9116ad6c48e91343c8086ca2
shedulervic <sheduler.vic@gmial.com>
2017-06-30 17:50:28 +0300

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.include Capybara::DSL
+  config.include Rails.application.routes.url_helpers
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
